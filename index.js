37d526b5859bd1aacd06706701b45de2bbe70b7c
Eiww <niktonikakoj0@gmail.com>
2025-01-20 09:10:47 +0900

    task 2

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+//task 1
+
 function multiply(a, b) {
   return a * b;
 }
 
 console.log(multiply(3, 8)); // 24
 console.log(multiply(1, 2)); // 2
+
+//task 2
+
+function repeat(text = String, count = Number) {
+  return text.repeat(count);
+}
+
+console.log(repeat("sometext", 2));
+console.log(repeat("wo", 3));
