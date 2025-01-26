4458afe93ecad38d5d6f84407a28b198a33bd4a9
Eiww <niktonikakoj0@gmail.com>
2025-01-26 15:50:35 +0900

    task 4 complete

--- index.js
+++ index.js
@@ -19,7 +19,21 @@ console.log(repeate("wo", 3));
 //task 3
 function getHiddenCard(card: string, count?: number) {
   const star = "*";
-  return `${star.repeat(count || 0)}${card.slice(12)}`;
+  return console.log(`${star.repeat(count || 0)}${card.slice(12)}`);
 }
 
-console.log(getHiddenCard("1234123412341234", 2));
+getHiddenCard("1234567812345678", 2); // "**5678"
+getHiddenCard("1234567812345678", 3); // "***5678"
+getHiddenCard("1234567812345678"); // "****5678"
+getHiddenCard("2034399002121100", 1); // "*1100"
+
+//task 4
+
+function getEvenNumbers(numbers: number[]): number[] {
+  return numbers.filter((num) => num % 2 === 0);
+}
+
+const numbers = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+console.log(getEvenNumbers(numbers));
