78b6286923ac9842786153d78aceb9ad37624529
Eiww <niktonikakoj0@gmail.com>
2025-01-26 15:28:02 +0900

    task 3 complete

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 //task 1
 
-function multiply(a, b) {
+function multiplyy(a, b) {
   return a * b;
 }
 
-console.log(multiply(3, 8)); // 24
-console.log(multiply(1, 2)); // 2
+console.log(multiplyy(3, 8)); // 24
+console.log(multiplyy(1, 2)); // 2
 
 //task 2
 
-function repeat(text = String, count = Number) {
+function repeate(text: string, count: number) {
   return text.repeat(count);
 }
 
-console.log(repeat("sometext", 2));
-console.log(repeat("wo", 3));
+console.log(repeate("sometext", 2));
+console.log(repeate("wo", 3));
 
-//task 3 в процессе
-
-function getHiddenCard(card = String, count = Number) {
+//task 3
+function getHiddenCard(card: string, count?: number) {
   const star = "*";
-  return `${star.repeat(count)} ${card.slice(12)}`;
+  return `${star.repeat(count || 0)}${card.slice(12)}`;
 }
 
 console.log(getHiddenCard("1234123412341234", 2));
